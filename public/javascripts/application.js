ac587e88d732c68e332c313551037b8b56dd0e04
Amber Feng <amber.feng@gmail.com>
2011-07-03 14:20:18 -0700

    Removed console.logs from application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,11 +7,9 @@ function getLocation() {
 }
 
 function lookupLocationName(position) {
-    console.log(position);
     $.post("/search/location", 
         {lat: position.coords.latitude, long: position.coords.longitude},  
         function(data) {
-          console.log(data.location);
           $("#current_location").text(data.location);
         }
     );
