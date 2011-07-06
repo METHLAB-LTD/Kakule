efb1bada4b16150f38715355e91b84debff4e778
Amber Feng <amber.feng@gmail.com>
2011-07-05 19:55:25 -0700

    Halfway through AJAX-ing sidebar with search, switching to working on calendar for now

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,9 @@
+kakule = {};
+kakule.current = {
+    lat: 0,
+    long: 0
+}
+
 function getLocation() {
   if (Modernizr.geolocation) {
     navigator.geolocation.getCurrentPosition(lookupLocationName);
@@ -7,15 +13,62 @@ function getLocation() {
 }
 
 function lookupLocationName(position) {
+    /* Save to temporary variables */
+    kakule.current.lat = position.coords.latitude;
+    kakule.current.long = position.coords.longitude;
+
+    /* Find location name */
     $.post("/search/location", 
-        {lat: position.coords.latitude, long: position.coords.longitude},  
+        {lat: kakule.current.lat, long: kakule.current.long},  
         function(data) {
           $("#current_location").text(data.location);
         }
     );
 }
 
+function openAction(search) {
+    return $("#" + search + " .search").css("display") != "none";
+}
+
+function searchLocations() {
+    return [];
+}
+
+function searchAttractions() {
+    $.post("/search/events",
+        {lat: kakule.current.lat, long: kakule.current.long}, 
+        function(data) {
+            repopulateList("attractions", data);
+        }
+    );
+}
+
+function searchMeals() {
+}
+
+function repopulateList(category, data) {
+    console.log(data);
+}
+
+function attachAddHandlers() {
+    $("#locations .heading").click(function() {
+        $("#locations .search").toggle();
+    });
+
+    $("#attractions .heading").click(function() {
+        $("#attractions .search").toggle();
+        if(openAction("attractions")) {
+            searchAttractions();
+        }
+    });
+
+    $("#meals .heading").click(function() {
+        $("#meals .search").toggle();
+    });
+}
+
 $(document).ready(function() {
     getLocation();
+    attachAddHandlers();
 });
 
