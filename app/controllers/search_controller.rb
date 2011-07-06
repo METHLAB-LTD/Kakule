81da282b76fd02b546faa56dfceede2923d90e7f
Anhang Zhu <anhang@gmail.com>
2011-07-05 20:33:12 -0700

    refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,23 +2,10 @@ class SearchController < ApplicationController
   
   
   # POST /search/events
-  # params => {"lat" : 25, "long" : -120, "radius" : 50, "query" : "ass", "start_time" : (new Date()).toLocaleString(), "end_time" : (new Date(2011,7,2)).toLocaleString()}
+  # params => {"lat" : 25, "long" : -120, "radius" : 50, "query" : "ass", "start_time" : (new Date()).toLocaleString(), "end_time" : (new Date(2011,7,2)).toLocaleString(), "limit" : 20}
   def events
     #validate_time_range
-    
-    params[:radius] = 10 if params[:radius].nil?
-    params[:end_time] = Time.now + 1.day if params[:end_time].nil?
-    params[:start_time] = Time.now if params[:start_time].nil?
-    
-    
-    @events = Event.find(:all, 
-      :conditions => ["(latitude between ? and ?) AND (longitude between ? and ?) 
-        AND ((name LIKE ?) OR (description LIKE ?)) 
-        AND (start_time < ? AND (end_time > ? OR end_time = ?))", 
-        params[:lat].to_i - params[:radius].to_i, params[:lat].to_i + params[:radius].to_i, params[:long].to_i - params[:radius].to_i, params[:long].to_i + params[:radius].to_i,
-        "%#{params[:query]}%",  "%#{params[:query]}%",
-        Time.parse(params[:end_time]), Time.parse(params[:start_time]), nil
-      ], :limit => 20)
+    @events = Event.find_by_custom_params(params)
     
     render :json => {
       :events => @events
