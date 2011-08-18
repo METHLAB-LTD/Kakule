f3ee7aaa2536fa52ef213b672b47c160de948a8f
Anhang Zhu <anhang@gmail.com>
2011-08-17 20:43:11 -0700

    search geocodes from beginning of string

--- db/migrate/20110803210628_add_description_to_geocodes.rb
+++ db/migrate/20110803210628_add_description_to_geocodes.rb
@@ -2,10 +2,14 @@ class AddDescriptionToGeocodes < ActiveRecord::Migration
   def self.up
     add_column :geocodes, :description, :text
     add_index :geocodes, :name, { :name => "geocodes_name_index" }
+    add_index :geocodes, :state, { :name => "geocodes_state_index" }
+    add_index :geocodes, :country, { :name => "geocodes_country_index" }
   end
 
   def self.down
     remove_column :geocodes, :description
-    remove_index :geocodes, "geocodes_name_index"
+    remove_index :geocodes, :name => "geocodes_name_index"
+    remove_index :geocodes, :name => "geocodes_state_index"
+    remove_index :geocodes, :name => "geocodes_country_index"
   end
 end
