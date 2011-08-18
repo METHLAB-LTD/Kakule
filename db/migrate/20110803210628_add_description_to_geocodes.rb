a291757985b4f4ed5483dab9a8c7d61f6eafbc52
Anhang Zhu <anhang@gmail.com>
2011-08-17 20:26:24 -0700

    added index to geocodes/name

--- db/migrate/20110803210628_add_description_to_geocodes.rb
+++ db/migrate/20110803210628_add_description_to_geocodes.rb
@@ -1,9 +1,11 @@
 class AddDescriptionToGeocodes < ActiveRecord::Migration
   def self.up
     add_column :geocodes, :description, :text
+    add_index :geocodes, :name, { :name => "geocodes_name_index" }
   end
 
   def self.down
     remove_column :geocodes, :description
+    remove_index :geocodes, "geocodes_name_index"
   end
 end
